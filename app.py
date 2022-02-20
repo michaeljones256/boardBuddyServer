f58223257694d49bfef5fbbc090d9b679a053546
Sal DeDona <sdedona2@verizon.net>
2022-02-20 01:37:47 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
+
+from vision import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/data")
+def data():
+    response = app.response_class(
+        response=json.dumps(detect_document("IMG_6361.jpg")),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
